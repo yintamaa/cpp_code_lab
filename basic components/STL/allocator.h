0075983979af690ce0de03a6e63a5d83179c8d22
bitsetli <bitsetli@tencent.com>
2022-11-17 21:05:58 +0800

    添加目录

--- basic components/STL/allocator.h
+++ basic components/STL/allocator.h
@@ -5,4 +5,18 @@
 #ifndef CPPTEST_ALLOCATOR_H
 #define CPPTEST_ALLOCATOR_H
 
+#include <new>
+#include <cstddef>
+#include <cstdlib>
+#include <climits>
+#include <iostream>
+
+namespace allocator {
+    template<class T>
+    inline T* allocate(ptrdiff_t size, T*) {
+        std::set_new_handler(0);
+        T *temp = (T*)(::operator new((size_t)(size * sizeof(T))));
+    }
+}
+
 #endif //CPPTEST_ALLOCATOR_H
